28f51247123ec538c5b8367bb7ad125937c854bf
Baratron <163884335+Baratron@users.noreply.github.com>
2025-04-22 19:48:45 +0300

    Add files via upload

--- poshelNAx.js
+++ poshelNAx.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('start').addEventListener('click', () => {
 
-        let cellCount = Math.ceil(getSurprise(5, 25));
+        let list = document.querySelectorAll('.item img');
+        for(let cell of list){
+            if(cell.style.display == "inline") cell.style.display = "none";
+        };
 
-        console.log(cellCount);
-
-        let cellNames = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24']
+        let cellCount = Math.ceil(getSurprise(1, 4));
+        let cellNames = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 
+            'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24'];
         
         for(let i = 0; i < cellCount; ++i){
-
-            let cellIndex = Math.ceil(getSurprise(0, cellCount));
+            let cellIndex = Math.ceil(getSurprise(0, (cellNames.length - 1)));
             let cellCoordinate = cellNames[cellIndex];
-            //cellNames = cellNames.splice(cellIndex, 1);
-            console.log(cellCoordinate);
-            document.getElementById(`${cellCoordinate}`).style.backgroundColor = "black";
-            
+            document.getElementById(`${cellCoordinate}`).querySelector('img').style.display = "inline";       
         };
         
         function getSurprise(min, max) {
